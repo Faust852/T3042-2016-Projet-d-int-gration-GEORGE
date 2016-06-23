12ba06ca6c20b26a8196aebea27fd8cd1115a12a
faust852 <gurafity@gmail.com>
2016-06-23 21:16:11 +0200

    Collision detection, map mouvement

---
+++ workspace/pr1/src/pr1/main/GameStateManager.java
@@ -0,0 +1,26 @@
+package pr1.main;
+
+import java.awt.Graphics2D;
+import java.util.Stack;
+
+public class GameStateManager {
+	
+	public static Stack<GameState> states;
+	
+	public GameStateManager() {
+		states = new Stack<GameState>();
+		states.push(new LevelLoader(this));
+	}
+	
+	public void tick(double deltaTime){
+		states.peek().tick(deltaTime);
+	}
+	
+	public void render(Graphics2D g){
+		states.peek().render(g);
+	}
+
+	public void init() {
+		states.peek().init();		
+	}
+}
