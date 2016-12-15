3f588ca5bb6b7dfcf3b44476ce54851b9779c43c
sponchau <levraipp7@outlook.be>
2016-12-15 12:29:32 +0100

    Update test.php

--- GeorgesWebsite/INC/test.php
+++ GeorgesWebsite/INC/test.php
@@ -2,35 +2,10 @@
 //use PHPUnit\Framework\TestCase;
 class test extends \PHPUnit_Framework_TestCase
 {
- 	function testRemplissageSession()
-		{
-		    fillSession();
-		   	$this->assertEquals($_SESSION['DB']['host'], "185.14.186.97");
-			$this->assertEquals($_SESSION['DB']['dbname'], 'georges');
-			$this->assertEquals($_SESSION['DB']['user'], 'root');
-		}
-	function testConnectionBDD ()
-		{
-			fillSession();
-			//ConnectBDD ();
-		}
 	function testPrintR ()
 		{
 			$tab['test'] = 'test';
 			$this->assertEquals(monPrint_r ($tab) , '<pre>'.print_r($tab, true).'</pre>');
-		}
-	function testChargeTemplate ()
-		{
-			$file = file('contact.template.inc.php');
-			implode ('', $file);
-			$this->assertEquals(implode ('', $file) , chargeTemplate ('contact'));
-			global $envoi;
-			chargeTemplate ('contact');
-			$this->assertEquals($envoi['sous-menu'] , creeMenu(['contact' => '' ]));
-		}
-	function testGetData ()	
-		{
-			$this->assertEquals(getData('coucou', 'test') , "[coucou][test] inconnu");
 		}
 } 
 
